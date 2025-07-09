23083319d1ef1e8ef9c28915708b4b68892e941a
RahebKarokhil <your_email@example.com>
2025-07-09 17:01:45 +0430

    Initial commit

---
+++ src/Commponint/Home/Exclusive/Images.jsx
@@ -0,0 +1,9 @@
+export const img = [
+  {
+    id: 1,
+    imgUrl: "../public/Exclusive/IMG-20250305-WA0048.jpg",
+  },
+
+  { id: 2,
+     imgUrl: "../public/Exclusive/div.property-banner-inner.png" },
+];
---
+++ src/Commponint/Home/Explor/Explordata.jsx
@@ -0,0 +1,35 @@
+export const explor = [
+  {
+    id: 1,
+    imgUrl: "../public/Explorimg/IMG-20250305-WA0030.jpg",
+    title: "2 Properties",
+    description: "Chicago",
+  },
+
+  {
+    id: 2,
+    imgUrl: "../public/Explorimg/IMG-20250305-WA0043.jpg",
+    title: "2 Properties",
+    description: "Washington DC",
+  },
+
+  {
+    id: 3,
+    imgUrl: "../public/Explorimg/IMG-20250305-WA0034.jpg",
+    title: "2 Properties",
+    description: "Newyourk",
+  },
+
+  {
+    id: 4,
+    imgUrl: "../public/Explorimg/IMG-20250305-WA0044.jpg",
+    title: "2 Properties",
+    description: "Calforinya",
+  },
+  {
+    id: 5,
+    imgUrl: "../public/Explorimg/IMG-20250305-WA0033.jpg",
+    title: "2 Properties",
+    description: "Calforinya",
+  },
+];
